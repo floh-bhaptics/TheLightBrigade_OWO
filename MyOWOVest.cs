3017080df5bf5cc4b7763aeadacb774ffb6a126a
floh-bhaptics <96761494+floh-bhaptics@users.noreply.github.com>
2024-04-25 12:26:27 +0200

    Add project files.

---
+++ MyOWOVest.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using MelonLoader;
+using OWOGame;
+//using MyOWOSensations;
+
+namespace MyOwoVest
+{
+    public class TactsuitVR
+    {
+        /* A class that contains the basic functions for the bhaptics Tactsuit, like:
+         * - A Heartbeat function that can be turned on/off
+         * - A function to read in and register all .tact patterns in the bHaptics subfolder
+         * - A logging hook to output to the Melonloader log
+         * - 
+         * */
+        public bool suitDisabled = true;
+        public bool systemInitialized = false;
+        // Event to start and stop the heartbeat thread
+        public Dictionary<String, Sensation> FeedbackMap = new Dictionary<String, Sensation>();
+
+        public TactsuitVR()
+        {
+            RegisterAllTactFiles();
+            InitializeOWO();
+        }
+
+        private async void InitializeOWO()
+        {
+            LOG("Initializing suit");
+
+            // New auth.
+            var gameAuth = GameAuth.Create(AllBakedSensations()).WithId("6278618");
+
+            OWO.Configure(gameAuth);
+            string[] myIPs = getIPsFromFile("OWO_Manual_IP.txt");
+            if (myIPs.Length == 0) await OWO.AutoConnect();
+            else
+            {
+                await OWO.Connect(myIPs);
+            }
+
+            if (OWO.ConnectionState == ConnectionState.Connected)
+            {
+                suitDisabled = false;
+                LOG("OWO suit connected.");
+            }
+            if (suitDisabled) LOG("Owo is not enabled?!?!");
+        }
+
+        public string[] getIPsFromFile(string filename)
+        {
+            List<string> ips = new List<string>();
+            string filePath = Directory.GetCurrentDirectory() + "\\Mods\\" + filename;
+            if (File.Exists(filePath))
+            {
+                LOG("Manual IP file found: " + filePath);
+                var lines = File.ReadLines(filePath);
+                foreach (var line in lines)
+                {
+                    IPAddress address;
+                    if (IPAddress.TryParse(line, out address)) ips.Add(line);
+                    else LOG("IP not valid? ---" + line + "---");
+                }
+            }
+            return ips.ToArray();
+        }
+
+        ~TactsuitVR()
+        {
+            LOG("Destructor called");
+            DisconnectOwo();
+        }
+
+        private BakedSensation[] AllBakedSensations()
+        {
+            var result = new List<BakedSensation>();
+
+            foreach (var sensation in FeedbackMap.Values)
+            {
+                if (sensation is BakedSensation baked)
+                {
+                    LOG("Registered baked sensation: " + baked.name);
+                    result.Add(baked);
+                }
+                else
+                {
+                    LOG("Sensation not baked? " + sensation);
+                    continue;
+                }
+            }
+            return result.ToArray();
+        }
+
+        public void DisconnectOwo()
+        {
+            LOG("Disconnecting Owo skin.");
+            OWO.Disconnect();
+        }
+
+        public void LOG(string logStr)
+        {
+            MelonLogger.Msg(logStr);
+        }
+
+        void RegisterAllTactFiles()
+        {
+
+            string configPath = Directory.GetCurrentDirectory() + "\\Mods\\OWO";
+            DirectoryInfo d = new DirectoryInfo(configPath);
+            FileInfo[] Files = d.GetFiles("*.owo", SearchOption.AllDirectories);
+            for (int i = 0; i < Files.Length; i++)
+            {
+                string filename = Files[i].Name;
+                string fullName = Files[i].FullName;
+                string prefix = Path.GetFileNameWithoutExtension(filename);
+                // LOG("Trying to register: " + prefix + " " + fullName);
+                if (filename == "." || filename == "..")
+                    continue;
+                string tactFileStr = File.ReadAllText(fullName);
+                try
+                {
+                    Sensation test = Sensation.Parse(tactFileStr);
+                    FeedbackMap.Add(prefix, test);
+                }
+                catch (Exception e) { LOG(e.ToString()); }
+
+            }
+
+            systemInitialized = true;
+        }
+
+        public string DetachFromMuscles(string pattern)
+        {
+            return System.Text.RegularExpressions.Regex.Replace(pattern, "\\|([0-9]%[0-9]+(,)*)+", "");
+        }
+
+        public void PlayBackHit(string pattern, float xzAngle, float yShift)
+        {
+            string postfix = "";
+            pattern = "Hit";
+            // two parameters can be given to the pattern to move it on the vest:
+            // 1. An angle in degrees [0, 360] to turn the pattern to the left
+            // 2. A shift [-0.5, 0.5] in y-direction (up and down) to move it up or down
+            if ((xzAngle < 90f))
+            {
+                postfix = "_FL";
+            }
+            if ((xzAngle > 90f) && (xzAngle < 180f))
+            {
+                postfix = "_BL";
+            }
+            if ((xzAngle > 180f) && (xzAngle < 270f))
+            {
+                postfix = "_BR";
+            }
+            if ((xzAngle > 270f))
+            {
+                postfix = "_FR";
+            }
+            string myPattern = pattern + postfix;
+            PlayBackFeedback(myPattern);
+
+        }
+
+        public void ArmHit(string pattern, bool isRightArm)
+        {
+            Sensation sensation = FeedbackMap[pattern];
+            if (sensation is SensationWithMuscles) { PlayBackFeedback(pattern); return; }
+            Muscle myMuscle = Muscle.Arm_L;
+            if (isRightArm) myMuscle = Muscle.Arm_R;
+            PlayBackFeedback(pattern, myMuscle);
+        }
+
+        public void GunRecoil(bool isRightHand, float intensity=1.0f, bool isTwoHanded=false, bool supportHand=true)
+        {
+            string pattern = "Recoil";
+            if (isTwoHanded)
+            {
+                pattern += "Twohanded";
+            }
+            if (isRightHand) pattern += "_R";
+            else pattern += "_L";
+            PlayBackFeedback(pattern);
+        }
+
+        public void SwordRecoil(bool isRightHand, float intensity = 1.0f)
+        {
+            if (isRightHand) PlayBackFeedback("Recoil_R");
+            else PlayBackFeedback("Recoil_L");
+        }
+
+        public void GrabAmmo(bool isRightHand)
+        {
+            if (isRightHand) PlayBackFeedback("GrabAmmo_R");
+            else PlayBackFeedback("GrabAmmo_L");
+        }
+
+        public void CastSpell(bool isRightHand)
+        {
+            if (isRightHand) PlayBackFeedback("Spell_R");
+            else PlayBackFeedback("Spell_L");
+        }
+
+        public void ShootBow(bool isRightHand)
+        {
+            if (isRightHand) PlayBackFeedback("ShootBow_R");
+            else PlayBackFeedback("ShootBow_L");
+        }
+
+        public void PlayBackFeedback(string feedback, float intensity=1.0f)
+        {
+            if (FeedbackMap.ContainsKey(feedback))
+            {
+                OWO.Send(FeedbackMap[feedback]);
+            }
+            else LOG("Feedback not registered: " + feedback);
+        }
+
+        public void PlayBackFeedback(string feedback, Muscle onMuscle)
+        {
+            if (FeedbackMap.ContainsKey(feedback))
+            {
+                OWO.Send(FeedbackMap[feedback].WithMuscles(onMuscle));
+            }
+            else LOG("Feedback not registered: " + feedback);
+        }
+
+        public void PlayBackFeedback(string feedback, Muscle[] onMuscles)
+        {
+            if (FeedbackMap.ContainsKey(feedback))
+            {
+                OWO.Send(FeedbackMap[feedback].WithMuscles(onMuscles));
+            }
+            else LOG("Feedback not registered: " + feedback);
+        }
+
+    }
+}
---
+++ TheLightBrigade_OWO.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using HarmonyLib;
+using MyOwoVest;
+using LB;
+using UnityEngine;
+using Unity.Mathematics;
+using System.Diagnostics;
+
+[assembly: MelonInfo(typeof(TheLightBrigade_OWO.TheLightBrigade_OWO), "TheLightBrigade_OWO", "1.0.0", "Florian Fahrenberger")]
+[assembly: MelonGame("Funktronic Labs", "The Light Brigade")]
+
+
+namespace TheLightBrigade_OWO
+{
+    public class TheLightBrigade_OWO : MelonMod
+    {
+        public static TactsuitVR tactsuitVr;
+        public static bool rightFoot = true;
+        public static bool rightHanded = true;
+        public static double lastFootStep = 0.0;
+        public static Stopwatch footStepTimer = new Stopwatch();
+        public static Unity.Mathematics.Random myRandom = new Unity.Mathematics.Random();
+
+        public override void OnInitializeMelon()
+        {
+            tactsuitVr = new TactsuitVR();
+        }
+
+        #region Weapons
+
+        [HarmonyPatch(typeof(Weapon_Rifle), "TryFire", new Type[] { })]
+        public class bhaptics_RifleFire
+        {
+            [HarmonyPrefix]
+            public static void Prefix(Weapon_Rifle __instance, bool ___boltOpenState, float ___nextShot, bool ___hammerOpenState)
+            {
+                if (___boltOpenState) { return; }
+                if (__instance.TypeOfWeapon != WeaponType.Pistol)
+                    if ((UnityEngine.Object)__instance.nodeHammer != (UnityEngine.Object)null && ___hammerOpenState) { return; }
+                if ((BaseConfig)__instance.chamber == (BaseConfig)null || __instance.chamberSpent) { return; }
+                bool isRight = __instance.grabTrigger.gripController.IsRightController();
+                bool twoHanded = false;
+                if ((UnityEngine.Object)__instance.grabBarrel != (UnityEngine.Object)null)
+                    if ((UnityEngine.Object)__instance.grabBarrel.gripController != (UnityEngine.Object)null)
+                        twoHanded = true;
+                //twoHanded = __instance.grabTrigger.alternateGrabAlso;
+                //twoHanded = (__instance.grabBarrel != null);
+                tactsuitVr.GunRecoil(isRight, 1.0f, twoHanded);
+            }
+        }
+
+        [HarmonyPatch(typeof(Weapon_Wand), "OnHeldTriggerRelease", new Type[] { typeof(XRController) })]
+        public class bhaptics_CastSpell
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Weapon_Wand __instance, XRController controller)
+            {
+                bool isRight = controller.IsRightController();
+                tactsuitVr.CastSpell(isRight);
+            }
+        }
+
+        [HarmonyPatch(typeof(Weapon_Bow), "OnGrabStopString", new Type[] { typeof(XRController) })]
+        public class bhaptics_ShootBow
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Weapon_Bow __instance, XRController controller)
+            {
+                bool isRight = controller.IsRightController();
+                tactsuitVr.ShootBow(!isRight);
+            }
+        }
+
+        [HarmonyPatch(typeof(Weapon_Blunt), "OnCollisionEnter", new Type[] { typeof(Collision) })]
+        public class bhaptics_SwordCollide
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Weapon_Blunt __instance, Collision collision)
+            {
+                float speed = collision.relativeVelocity.magnitude;
+                tactsuitVr.SwordRecoil(true, speed / 10.0f);
+            }
+        }
+
+        [HarmonyPatch(typeof(InventorySlot), "OnStoreItemFX", new Type[] { })]
+        public class bhaptics_StoreInventory
+        {
+            [HarmonyPostfix]
+            public static void Postfix(InventorySlot __instance)
+            {
+                if (__instance.inventorySlotType == InventorySlotType.Rifle) tactsuitVr.PlayBackFeedback("RifleStore");
+                if (__instance.inventorySlotType == InventorySlotType.Ammo)
+                {
+                    if (rightHanded) tactsuitVr.PlayBackFeedback("StoreAmmo_L");
+                    else tactsuitVr.PlayBackFeedback("StoreAmmo_R");
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(InventorySlot), "OnUnstoreItemFX", new Type[] { })]
+        public class bhaptics_ReceiveInventory
+        {
+            [HarmonyPostfix]
+            public static void Postfix(InventorySlot __instance)
+            {
+                if (__instance.inventorySlotType == InventorySlotType.Rifle) tactsuitVr.PlayBackFeedback("RifleReceive");
+                if (__instance.inventorySlotType == InventorySlotType.Ammo)
+                {
+                    if (rightHanded) tactsuitVr.PlayBackFeedback("ReceiveAmmo_L");
+                    else tactsuitVr.PlayBackFeedback("ReceiveAmmo_R");
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(InventoryRoot), "SetHandednessPoses", new Type[] { typeof(Handedness) })]
+        public class bhaptics_SetHandedness
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Handedness handedness)
+            {
+                rightHanded = (handedness == Handedness.Right);
+            }
+        }
+        #endregion
+
+        #region Damage and Health
+
+        private static (float, float) getAngleAndShift(Transform player, Vector3 hitPoint, Quaternion headRotation)
+        {
+            Vector3 patternOrigin = new Vector3(0f, 0f, 1f);
+            // y is "up", z is "forward" in local coordinates
+            Vector3 hitPosition = hitPoint - player.position;
+            Quaternion PlayerRotation = player.rotation * headRotation;
+            Vector3 playerDir = PlayerRotation.eulerAngles;
+            // We only want rotation correction in y direction (left-right), top-bottom and yaw we can leave
+            Vector3 flattenedHit = new Vector3(hitPosition.x, 0f, hitPosition.z);
+            float earlyhitAngle = Vector3.Angle(flattenedHit, patternOrigin);
+            Vector3 earlycrossProduct = Vector3.Cross(flattenedHit, patternOrigin);
+            if (earlycrossProduct.y > 0f) { earlyhitAngle *= -1f; }
+            float myRotation = earlyhitAngle - playerDir.y;
+            myRotation *= -1f;
+            if (myRotation < 0f) { myRotation = 360f + myRotation; }
+
+            float hitShift = hitPosition.y;
+            //tactsuitVr.LOG("Hitshift: " + hitShift.ToString());
+            float upperBound = 1.7f;
+            float lowerBound = 1.2f;
+            if (hitShift > upperBound) { hitShift = 0.5f; }
+            else if (hitShift < lowerBound) { hitShift = -0.5f; }
+            // ...and then spread/shift it to [-0.5, 0.5], which is how bhaptics expects it
+            else { hitShift = (hitShift - lowerBound) / (upperBound - lowerBound) - 0.5f; }
+
+            return (myRotation, hitShift);
+        }
+
+        [HarmonyPatch(typeof(PlayerActor), "OnDamageApply", new Type[] { typeof(ProjectileHitInfo), typeof(ProjectileService.DamageResult) })]
+        public class bhaptics_PlayerHit
+        {
+            [HarmonyPostfix]
+            public static void Postfix(PlayerActor __instance, ProjectileHitInfo info, ProjectileService.DamageResult damageResult)
+            {
+                Quaternion headRotation = __instance.headRotation();
+                float hitAngle;
+                float hitShift;
+                string damageType = "BulletHit";
+                if (info.damageData.instantKill) damageType = "Impact";
+                if (info.damageData.isExplodingShot) damageType = "Impact";
+                if ((info.damageData.isExplosion) || (info.damageData.isGrenade)) { tactsuitVr.PlayBackFeedback("Explosion"); return; }
+                if (info.damageData.isMelee) damageType = "BladeHit";
+                if (info.damageData.isPoison) damageType = "Poison";
+                if (info.damageData.isSpell) damageType = "Impact";
+                (hitAngle, hitShift) = getAngleAndShift(__instance.transform, info.hitPosition, headRotation);
+                tactsuitVr.PlayBackHit(damageType, hitAngle, hitShift);
+            }
+        }
+
+
+        [HarmonyPatch(typeof(Consumable_Medkit), "OnConsumableApplyEffect", new Type[] { typeof(Actor) })]
+        public class bhaptics_PlayerMedKit
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.PlayBackFeedback("Healing");
+            }
+        }
+
+        #endregion
+
+        #region Extra effects
+
+
+        [HarmonyPatch(typeof(JuiceVolume), "FlashSettings", new Type[] { typeof(JuiceVolume.JuiceLayerName), typeof(float), typeof(float), typeof(float) })]
+        public class bhaptics_Prayer
+        {
+            [HarmonyPostfix]
+            public static void Postfix(JuiceVolume __instance, JuiceVolume.JuiceLayerName layerName, float fadeInSecs, float holdDurationSecs, float fadeOutSecs)
+            {
+                if (layerName == JuiceVolume.JuiceLayerName.PlayerTeleport) tactsuitVr.PlayBackFeedback("Teleport");
+                if (layerName == JuiceVolume.JuiceLayerName.AbsorbSoul) tactsuitVr.PlayBackHit("AbsorbSoul", myRandom.NextFloat(0f, 360f), myRandom.NextFloat(-0.4f, 0.4f));
+                if (layerName == JuiceVolume.JuiceLayerName.LevelUp) tactsuitVr.PlayBackFeedback("LevelUp");
+                if (layerName == JuiceVolume.JuiceLayerName.AbsorbTarotCard) tactsuitVr.PlayBackFeedback("AbsorbTarotCard");
+                if (layerName == JuiceVolume.JuiceLayerName.Prayer)
+                {
+                    if ((fadeInSecs == 0.1f) && (fadeOutSecs == 0.3f)) return;
+                    //tactsuitVr.LOG("Numbers: " + fadeInSecs + " " + holdDurationSecs + " " + fadeOutSecs);
+                    tactsuitVr.PlayBackFeedback("Prayer");
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(JuiceVolume), "TriggerAreaClearRingBurst", new Type[] { })]
+        public class bhaptics_AreaClear
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.PlayBackFeedback("AreaClear");
+            }
+        }
+
+        [HarmonyPatch(typeof(JuiceVolume), "FadeOut", new Type[] { typeof(JuiceVolume.JuiceLayerName), typeof(float), typeof(float) })]
+        public class bhaptics_FadeInLayer
+        {
+            [HarmonyPostfix]
+            public static void Postfix(JuiceVolume __instance, JuiceVolume.JuiceLayerName layerName)
+            {
+                if (layerName == JuiceVolume.JuiceLayerName.PlayerTeleport)
+                {
+                    tactsuitVr.PlayBackFeedback("Teleport");
+                }
+            }
+        }
+
+        #endregion
+
+    }
+}
